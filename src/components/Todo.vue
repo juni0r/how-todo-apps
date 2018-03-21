025b0786af7cb7d17492b1618abbf4eea6a084c8
Andreas Korth <andreas.korth@neofonie.de>
2018-03-21 19:24:22 +0100

    Add Todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="todo">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  props: { todo: Object },
+  computed: {
+    completed() {
+      return this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
